b10c7c850f6eeb971992fb26870d56003e76bbfb
DevDiegoVillalobos <83984660+DiegoAraithVillalobosHernandez@users.noreply.github.com>
2022-12-07 14:30:35 -0600

    [UPDATE] views and semi responsive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,11 @@ import { AppComponent } from './app.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { MenubarModule } from 'primeng/menubar';
-
+import { Error404Component } from './components/error404/error404.component';
+import { Error403Component } from './components/error403/error403.component';
+import { Error500Component } from './components/error500/error500.component';
+import { DetailsComponent } from './components/details/details.component';
+import { MatIconModule} from '@angular/material/icon';
 
 
 @NgModule({
@@ -13,11 +17,16 @@ import { MenubarModule } from 'primeng/menubar';
     AppComponent,
     FooterComponent,
     HeaderComponent,
+    Error404Component,
+    Error403Component,
+    Error500Component,
+    DetailsComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MenubarModule
+    MenubarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/error403/error403.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-error403',
+  templateUrl: './error403.component.html',
+  styleUrls: ['./error403.component.css']
+})
+export class Error403Component {
+
+}
