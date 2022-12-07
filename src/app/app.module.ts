9f2df66804b05b61c8de632019026d63a0f0c629
Alan Yahir <alan.yahir.borjes@gmail.com>
2022-12-07 10:53:03 -0600

    Avance de los forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { MenubarModule } from 'primeng/menubar';
+import {FormsComponent} from './components/forms/forms.component';
 
 
 
@@ -13,6 +14,7 @@ import { MenubarModule } from 'primeng/menubar';
     AppComponent,
     FooterComponent,
     HeaderComponent,
+    FormsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/forms/forms.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-forms',
+  templateUrl: './forms.component.html',
+  styleUrls: ['./forms.component.css', '../../../../node_modules/bootstrap/dist/css/bootstrap-utilities.min.css']
+  
+})
+export class FormsComponent {
+
+}
