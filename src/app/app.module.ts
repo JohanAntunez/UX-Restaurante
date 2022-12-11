49b1ae1c06452377698958bd2344b22bde5f2175
AlanBorjes <20203tn059@utez.edu.mx>
2022-12-11 12:38:39 -0600

    Avance perdido

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,9 @@ import { HeaderComponent } from './components/header/header.component';
 import { MenubarModule } from 'primeng/menubar';
 import {FormsComponent} from './components/forms/forms.component';
 import {checkoutComponent} from './components/checkout/checkout.component'
-
+import {CdkStepperModule} from '@angular/cdk/stepper';
+import {ReactiveFormsModule } from '@angular/forms';
+import {MatProgressBarModule} from '@angular/material/progress-bar'
 
 @NgModule({
   declarations: [
@@ -20,7 +22,10 @@ import {checkoutComponent} from './components/checkout/checkout.component'
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MenubarModule
+    MenubarModule,
+    CdkStepperModule,
+    ReactiveFormsModule,
+    MatProgressBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
